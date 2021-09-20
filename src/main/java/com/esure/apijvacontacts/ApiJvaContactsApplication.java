c43f8cca3ed2bf7cf7f718e3240eb764636f7800
Tim Hood <tim.hood@esure.com>
2021-09-20 11:32:27 +0100

    first commit

---
+++ src/main/java/com/esure/apijvacontacts/ApiJvaContactsApplication.java
@@ -0,0 +1,13 @@
+package com.esure.apijvacontacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJvaContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJvaContactsApplication.class, args);
+	}
+
+}
